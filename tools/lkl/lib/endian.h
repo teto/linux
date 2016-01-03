7a62c02d0905cd8ccf35e648ddf9db951bc27be6
Octavian Purdila <octavian.purdila@intel.com>
2016-01-04 01:25:33 +0200

    lkl tools: host lib: fix hton*/ntoh*
    Signed-off-by: Octavian Purdila <octavian.purdila@intel.com>

--- tools/lkl/lib/endian.h
+++ tools/lkl/lib/endian.h
@@ -8,16 +8,21 @@
 #include <endian.h>
 #endif  /* __FreeBSD__ */
 #else  /* !__MINGW32__ */
+#include <winsock.h>
 #define le32toh(x) (x)
 #define le16toh(x) (x)
 #define htole32(x) (x)
 #define htole16(x) (x)
 #define le64toh(x) (x)
+#define htobe32(x) htonl(x)
+#define htobe16(x) htons(x)
+#define be32toh(x) ntohl(x)
+#define be16toh(x) ntohs(x)
 #endif  /* __MINGW32__ */
 
-#define htonl(x) htole32(x)
-#define htons(x) htole16(x)
-#define ntohl(x) le32toh(x)
-#define ntohs(x) le16toh(x)
+#define htonl(x) htobe32(x)
+#define htons(x) htobe16(x)
+#define ntohl(x) be32toh(x)
+#define ntohs(x) be16toh(x)
 
 #endif /* _LKL_LIB_ENDIAN_H */
